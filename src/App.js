722e9583f69c669c56e75ee760458fe363b57907
ocostalucas <ocostalucas.dev@gmail.com>
2024-11-28 10:39:41 -0300

    feat: Adicionando componente time

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import Time from "./components/Time";
 
 function App() {
 
@@ -11,6 +12,9 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador]) }/>
+      <Time nome="Programação"/>
+      <Time nome="Front-end"/>
+      <Time nome="Data Science"/>
     </div>
   );
 }
