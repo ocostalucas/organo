8e3f6c067b1084d32428356a8b5a52cb23d78cf4
ocostalucas <ocostalucas.dev@gmail.com>
2024-11-28 11:19:33 -0300

    feat: Adicionando componente colaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = (props) => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/ocostalucas.png' alt='Lucas Costa'></img>
+            </div>
+            <div className='rodape'>
+                <h4>Lucas Costa</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
